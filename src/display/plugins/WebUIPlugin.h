8439da9858b2d7d580707a7ff8312bca3fd6ea24
tobiasguyer <tobiasguyer23@gmail.com>
2026-07-02 10:07:11 +0200

    fix: link ota to /tobiasguyer/gaggimate

--- src/display/plugins/WebUIPlugin.h
+++ src/display/plugins/WebUIPlugin.h
@@ -17,7 +17,7 @@ constexpr size_t STATUS_PERIOD = 500;
 constexpr size_t DNS_PERIOD = 50;
 
 const String LOCAL_URL = "http://4.4.4.1/";
-const String RELEASE_URL = "https://github.com/jniebuhr/gaggimate/releases/";
+const String RELEASE_URL = "https://github.com/tobiasguyer/gaggimate/releases/";
 
 class ProfileManager;
 
